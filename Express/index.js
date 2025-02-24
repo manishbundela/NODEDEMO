2c24f6c1df9b5c6ce933c07269d8a7e18acefb45
manishbundela <manishsunrays@gmail.com>
2025-02-24 13:28:19 +0530

    done

--- Express/index.js
+++ Express/index.js
@@ -1,6 +1,10 @@
 const express=require('express');
 const app= express();
 
+const cors=require('cors');
+
+app.use(cors()); // used for remove across-origin issue from browser side.
+
 app.get('/',(req,resp)=>{
   resp.send('hi i am here');  
 });
